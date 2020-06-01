2b8a185a8497ca33133aea46c6a2a5faf11a30e5
LUANGKHOT Nalongsak (Loong) <loongluangkhot@gmail.com>
2020-06-01 11:25:09 +0800

    Changes in package.json; Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import { RestylingDemo } from './RestylingDemo';
-// import { css } from '@emotion/core';
+import { Layout, Menu } from 'antd';
 
-
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      // <div className="App">
-      // </div>
-      <RestylingDemo />
+      <div className="App">
+        <Layout className = "layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Menu #1</Menu.Item>
+              <Menu.Item key="2">Menu #2</Menu.Item>
+              <Menu.Item key="3">Menu #3</Menu.Item>
+            </Menu>
+          </Header>
+        </Layout>
+      </div>
     );
   }
 }
